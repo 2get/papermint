5384e92024b491b55b95b8fbc050d0f60ddd8383
Mayoto Yoshida <2getjp@gmail.com>
2012-06-12 11:48:23 +0900

    userとtaskを関連付け

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
   attr_accessible :content, :limit_date, :priority, :user_id
+
+  belongs_to :user
+
+  validates :content, :length => { :maximum => 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  has_many :tasks
 end
